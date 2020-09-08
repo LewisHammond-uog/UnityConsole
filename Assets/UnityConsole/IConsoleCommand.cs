3a0190b62d0999d755b883d4b7275b9076d16577
Lewis Hammond <s1800707@glos.ac.uk>
2020-09-08 17:01:12 +0100

    Fixed Naming of CommandResponse variables
    - Made CommandResponse Message a get only property
    - Fixed the naming of CommandResponse variables

--- Assets/UnityConsole/IConsoleCommand.cs
+++ Assets/UnityConsole/IConsoleCommand.cs
@@ -26,18 +26,18 @@ namespace Lewis.DevConsole
             PASS = 1
         }
 
-        public ResponseType type { get; } //Pass or fail
-        public string message; //Message associated with the pass or fail
+        public ResponseType Type { get; } //Pass or fail
+        public string Message { get; } //Message associated with the pass or fail
 
         //Constructor, must define type message can be left for passes
         public CommandResponse(ResponseType type, string message = "")
         {
             //Set Values
-            this.type = type;
-            this.message = message;
+            this.Type = type;
+            this.Message = message;
 
             //Throw a warning if we have a fail and do no provide a reason
-            if(this.type == ResponseType.FAIL && message == "")
+            if(this.Type == ResponseType.FAIL && message == "")
             {
                 Debug.LogWarning("Fail CommandRespose created without a reason");
             }

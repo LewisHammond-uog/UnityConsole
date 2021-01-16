7dfff8981954716e9bb543797d71a852f50ff556
Lewis Hammond <s1800707@connect.glos.ac.uk>
2021-01-16 18:01:48 +0000

    Basic Test Working
    Basic Test of Getting the project info from the HacknPlan website

---
+++ Assets/UnityConsole/Scripts/HacknPlanAPI/HNPTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+//5a1b44d8a798498b9058d0953c63d57c
+public class HNPTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(PostBugToAPI());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator PostBugToAPI()
+    {
+        var webRequest = UnityWebRequest.Get("https://api.hacknplan.com/v0/projects");
+        SetRequestHeaders(ref webRequest);
+
+        yield return webRequest.SendWebRequest();
+
+        string reponse = webRequest.downloadHandler.text;
+        Debug.Log(reponse);
+    }
+
+    /// <summary>
+    /// Set the Headers WebRequest
+    /// Sets the content type and Authorization headers
+    /// </summary>
+    private void SetRequestHeaders(ref UnityWebRequest request)
+    {
+        request.SetRequestHeader("Content-Type", "application/json");
+        request.SetRequestHeader("Authorization", "ApiKey {API KEY}");
+    }
+    
+    
+}
